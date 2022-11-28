36d74d3969d7721f5bca97a9667f1a8f8cbf250c
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-11-28 14:52:44 +0100

    added copy to clipboard

--- apps/graph-notify/src/components/SubgraphsDashboard.tsx
+++ apps/graph-notify/src/components/SubgraphsDashboard.tsx
@@ -2,24 +2,31 @@ import { useGraphNotifyStore } from "../store";
 import NoDataAlert from "./subgraphs-table/NoDataAlert";
 import { SubgraphTable } from "./subgraphs-table/SubgraphTable";
 import { encode } from "js-base64";
-import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import { FiShare2 } from "react-icons/fi";
 
 export const SubgraphsDashboard = () => {
-  const router = useRouter();
-
   const { inputs } = useGraphNotifyStore((state) => ({
     inputs: state.subgraphs,
   }));
 
   const onClickHandler = () => {
     const hash = encode(JSON.stringify(inputs));
+    navigator.clipboard.writeText(`${window.location.href}share/${hash}`);
 
-    router.push(`/share/${hash}`);
+    toast.success(`URL copied to clipboard 👏`);
   };
 
   return (
     <div className={"flex flex-col w-full pt-14"}>
-      <button onClick={onClickHandler}>SHARE</button>
+      <button
+        onClick={onClickHandler}
+        className="w-48 self-end btn btn-info btn-outline flex items-center my-2"
+      >
+        Share table
+        <FiShare2 className="ml-2" />
+      </button>
+
       {inputs.length ? <SubgraphTable inputs={inputs} /> : <NoDataAlert />}
     </div>
   );
